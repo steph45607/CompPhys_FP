2686cf9f9a323ff83479133c33ea6319dfea02f7
fl-sll <edwardalvin@gmail.com>
2023-05-30 19:10:54 +0700

    added user input

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import matplotlib.pyplot as plt
-from solar import Solar,SolarSystemBody,Sun,Planet
+from solar import Solar,Sun,Planet
 
 solarsys = Solar(400, projection_2d=True)
 
 sun = Sun(solarsys)
-planets = (
-    Planet(
-        solarsys,
-        position=(150, 50, 0),
-        velocity=(0, 5, 5),
-    ),
-    Planet(
-        solarsys,
-        mass=20,
-        position=(100, -50, 150),
-        velocity=(5, 0, 0)
-    )
-)
+planets = []
+
+n = int(input("How many planets do you want to add? "))
+
+for i in range(n):
+    m = int(input(f"Input planet {i+1}'s mass: "))
+    px = int(input(f"Input planet {i+1}'s x-coordinate: "))
+    py = int(input(f"Input planet {i+1}'s y-coordinate: "))
+    pz = int(input(f"Input planet {i+1}'s z-coordinate: "))
+    vx = int(input(f"Input planet {i+1}'s x-velocity: "))
+    vy = int(input(f"Input planet {i+1}'s y-velocity: "))
+    vz = int(input(f"Input planet {i+1}'s z-velocity: "))
+    planets.append(Planet(solarsys,mass=m,position=(px,py,pz),velocity=(vx,vy,vz)))
+
+print(planets)
+
 while True:
     solarsys.calculate_all_body_interactions()
     solarsys.update_all()
